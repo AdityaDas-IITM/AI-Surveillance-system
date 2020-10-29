941320e77288bf87eebea903d1a8a9057be8b687
Aditya Das <64326826+AdityaDas-IITM@users.noreply.github.com>
2020-10-30 00:11:00 +0530

    Update notif.py

--- scripts/notif.py
+++ scripts/notif.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 import time
 
-<<<<<<< HEAD
-
-=======
 joy_phone = "+919740718396"
 joy_message = 'Nigal'
 
@@ -13,22 +10,19 @@ pand_message = 'nibbi'
 vid_path = "C:/Users/nihal/Pictures/Camera Roll/hand_track_output.mp4"
 
 wapp = "https://web.whatsapp.com"
->>>>>>> 2cc930391f5ea32a28d8aa27f5a2a2c66432dd5e
 
 def send_vid(url, phone):
 
-<<<<<<< HEAD
     wapp = "https://web.whatsapp.com"
     url1 = 'https://web.whatsapp.com/send?phone='+phone+'&text='+'Alert'
     driver.get(wapp) 
-=======
+
 # load web whatsapp main page
 driver.get(wapp) 
 time.sleep(15)
 
 def send_vid(phone, message, file=''):
     url = 'https://web.whatsapp.com/send?phone='+phone+'&text='+message
->>>>>>> 2cc930391f5ea32a28d8aa27f5a2a2c66432dd5e
     # load the person's chat
     driver.get(url)
     time.sleep(5)
@@ -37,7 +31,6 @@ def send_vid(phone, message, file=''):
     driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button").click()
     time.sleep(0.1)
 
-<<<<<<< HEAD
     # attachment button click
     driver.find_element_by_css_selector("span[data-icon='clip']").click()
     time.sleep(0.1)
@@ -62,7 +55,7 @@ if __name__ == '__main__':
     time.sleep(15)
     send_vid(url1)  # joy
     send_vid(url2)  # pandey
-=======
+
     if file != '':
         # attachment button click
         driver.find_element_by_css_selector("span[data-icon='clip']").click()
@@ -76,4 +69,3 @@ if __name__ == '__main__':
 
 send_vid(joy_phone, joy_message, vid_path)  # joy
 send_vid(pand_phone, pand_message, vid_path)  # pandey
->>>>>>> 2cc930391f5ea32a28d8aa27f5a2a2c66432dd5e
