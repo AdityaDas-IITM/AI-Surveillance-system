356125b8d331d2b103abd587d9f55b98f452b3b0
nihalgeorge01 <nihalgeorge01@gmail.com>
2020-10-29 23:22:41 +0530

    Refactor Notif

--- scripts/notif.py
+++ scripts/notif.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 import time
 
-phone = "+919740718396"
-message = 'Nigal'
+joy_phone = "+919740718396"
+joy_message = 'Nigal'
+
+pand_phone = "+918105632052"
+pand_message = 'nibbi'
+
+vid_path = "C:/Users/nihal/Pictures/Camera Roll/hand_track_output.mp4"
 
 wapp = "https://web.whatsapp.com"
-url1 = 'https://web.whatsapp.com/send?phone='+phone+'&text='+message
-url2 = 'https://web.whatsapp.com/send?phone=+918105632052&text=nibbi'
 
 driver = webdriver.Chrome(executable_path='C:/Users/nihal/Downloads/chromedriver.exe')
 
@@ -14,7 +17,8 @@ driver = webdriver.Chrome(executable_path='C:/Users/nihal/Downloads/chromedriver
 driver.get(wapp) 
 time.sleep(15)
 
-def send_vid(url):
+def send_vid(phone, message, file=''):
+    url = 'https://web.whatsapp.com/send?phone='+phone+'&text='+message
     # load the person's chat
     driver.get(url)
     time.sleep(5)
@@ -23,15 +27,16 @@ def send_vid(url):
     driver.find_element_by_xpath("/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[3]/button").click()
     time.sleep(2)
 
-    # attachment button click
-    driver.find_element_by_css_selector("span[data-icon='clip']").click()
-    time.sleep(2)
-    # add file to send by file path
-    driver.find_element_by_css_selector("input[type='file']").send_keys("C:/Users/nihal/Pictures/Camera Roll/hand_track_output.mp4")
-    time.sleep(2)
-    # click to send
-    driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div").click()
-    time.sleep(10)
+    if file != '':
+        # attachment button click
+        driver.find_element_by_css_selector("span[data-icon='clip']").click()
+        time.sleep(2)
+        # add file to send by file path
+        driver.find_element_by_css_selector("input[type='file']").send_keys("C:/Users/nihal/Pictures/Camera Roll/hand_track_output.mp4")
+        time.sleep(2)
+        # click to send
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div").click()
+        time.sleep(10)
 
-send_vid(url1)  # joy
-send_vid(url2)  # pandey
+send_vid(joy_phone, joy_message, vid_path)  # joy
+send_vid(pand_phone, pand_message, vid_path)  # pandey
